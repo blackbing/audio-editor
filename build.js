e71e56c94381f61fc05c750b4f434258919f7ad3
Bingo Yang <blackbing@gmail.com>
2012-10-02 04:32:48 -0400

    fix build

--- build.js
+++ build.js
@@ -1,7 +1,16 @@
 {
-  "baseUrl":'javascript',
+  "baseUrl":'javascripts',
+  hbs : {
+    templateExtension : 'hbs',
+    disableI18n : true
+  },
+  paths  : {
+    'hbs' : 'vender/require-handlebars-plugin/hbs',
+    'handlebars' : 'vender/require-handlebars-plugin/Handlebars',
+    "underscore" : "vender/require-handlebars-plugin/hbs/underscore",
+    "i18nprecompile" : "vender/require-handlebars-plugin/hbs/i18nprecompile",
+    "json2" : "vender/require-handlebars-plugin/hbs/json2"
+  },
   name: "main",
-  out: 'javascript/main-built.js',
-  paths: {
-  }
+  out: 'javascript/main-built.js'
 }
